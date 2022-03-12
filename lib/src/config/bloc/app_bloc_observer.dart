719422fa0c7adf30e87a702b3460d2e99c0f6224
Vi Hoang <vihoang@Vis-MacBook-Pro.local>
2022-03-12 08:31:28 +0700

    Base core

---
+++ lib/src/config/bloc/app_bloc_observer.dart
@@ -0,0 +1,17 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    log('onChange(${bloc.runtimeType}, $change)');
+    super.onChange(bloc, change);
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    super.onError(bloc, error, stackTrace);
+  }
+}
